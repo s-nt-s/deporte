da1c98deba8967c3c435ceabe64d73cd3dbfdd8a
s-nt-s <santos82h@gmail.com>
2017-11-09 14:31:10 +0100

    v2.1

---
+++ README.md
@@ -0,0 +1,5 @@
+Recupera los horarios libres de varios deportivos, los filtra y los
+cruza con los datos meteorológicos (cuando se trata de una actividad al
+aire libre), para facilitar decidir que actividad realizar.
+
+Deja el resultado en [pez.000webhostapp.com/dp](https://pez.000webhostapp.com/dp/)
---
+++ weather.py
@@ -0,0 +1,43 @@
+import xml.etree.ElementTree as etree
+
+import requests
+
+class Weather():
+
+    def __init__(self, url):
+        response = requests.get(url)
+        self.tree = etree.fromstring(response.content)
+
+    def day_hour(self, day, hour):
+        if isinstance(hour, str):
+            hour = int(hour.split(":")[0])
+        data = {}
+        for t in self.tree.findall('.//prediccion/dia[@fecha="' + day + '"]/*'):
+            if not t.text:
+                continue
+            if "periodo" in t.attrib:
+                hour1, hour2 = t.attrib["periodo"].split("-")
+                if hour >= int(hour1) or hour < int(hour2):
+                    data[t.tag] = t.text
+                    if t.tag == "viento":
+                        data[t.tag] = t.find("velocidad").text
+                    if "descripcion" in t.attrib:
+                        data[t.tag + "_des"] = t.attrib["descripcion"]
+                continue
+            for d in t.findall("dato"):
+                if "hora" in d.attrib:
+                    hour1 = d.attrib["hora"]
+                    if hour >= int(hour1):
+                        data[t.tag] = d.text
+        for key, value in data.items():
+            if value.isdigit():
+                data[key] = int(value)
+
+        return data
+
+
+class WeatherMadrid(Weather):
+
+    def __init__(self):
+        super().__init__(
+            "http://www.aemet.es/xml/municipios/localidad_28079.xml")
