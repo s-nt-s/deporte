00b76669b31303032f8388c6e07485eddacb40b7
s-nt-s <santos82h@gmail.com>
2017-11-09 14:30:56 +0100

    v2.1

--- deporte.py
+++ deporte.py
@@ -1,13 +1,21 @@
+#!/usr/bin/env python3
+
 import calendar
 import json
 import locale
 import os
+import xml.etree.ElementTree as etree
 from datetime import date, datetime
+from ftplib import FTP
 
+import requests
 from jinja2 import Environment, FileSystemLoader
 
-from api import DESAdapter, Portal
 import holidays
+from crontab import CronTab
+
+from api import DESAdapter, Portal
+from weather import WeatherMadrid
 
 abspath = os.path.abspath(__file__)
 dname = os.path.dirname(abspath)
@@ -18,12 +26,14 @@ locale.setlocale(locale.LC_TIME, "es_ES.utf8")
 H_MIN = 15.3
 H_MAX = 20.0
 
-W_MIN = 1
-W_MAX = 4
+W_MIN = 2
+W_MAX = 5
 
 week = list(calendar.day_name)
 now = datetime.now()
 holi = holidays.ES(prov="MAD", years=now.year)
+weather = WeatherMadrid()
+out_file = "out/index.html"
 
 
 def in_range(fch):
@@ -32,10 +42,10 @@ def in_range(fch):
         return False
     if dt in holi:
         return True
-    w = dt.weekday()
-    if w in (5, 6):
+    w = dt.isoweekday()
+    if w in (6, 7):
         return True
-    #if w < W_MIN or w > W_MAX:
+    # if w < W_MIN or w > W_MAX:
     #    return False
     h, m = fch.split(" ")[1].split(":")
     tm = int(h) + (int(m) / 100)
@@ -49,7 +59,7 @@ def to_hh_mm(time):
 
 
 def get_paul():
-    userpass = open(".userpass").read().strip()
+    userpass = open(".ig_madrid.org").read().strip()
     user, password = userpass.split(" ")
     p = Portal("https://gestiona.madrid.org/cronosweb",
                user=user, password=password)
@@ -80,6 +90,29 @@ def get_mina_squash():
     return free
 
 
+def get_class(w_day_hour):
+    cls = ""
+    if w_day_hour:
+        return ""
+        if w_day_hour.get("prob_precipitacion", 0) > 50:
+            cls += " heavy_rain"
+        elif w_day_hour.get("prob_precipitacion", 0) > 5:
+            cls += " soft_rain"
+
+        if w_day_hour.get("viento", 0) > 20:
+            cls += " heavy_wind"
+        elif w_day_hour.get("viento", 0) > 5:
+            cls += " soft_wind"
+
+        if w_day_hour.get("sens_termica", 0) > 30:
+            cls += " heavy_hot"
+        elif w_day_hour.get("sens_termica", 0) < 20:
+            cls += " heavy_cold"
+    if len(cls) > 1:
+        cls = "weather " + cls
+    return cls.strip()
+
+
 def get_week_day(d):
     dt = datetime.strptime(d, '%Y-%m-%d')
     return dt.strftime("%A").title()
@@ -96,24 +129,47 @@ def to_pm(hm, short=False):
     return "%s:%s pm" % (h, m)
 
 
-def build_times(free):
+def build_times(free, set_weather=False):
     splt = [f.split(" ") for f in free]
     days = sorted(set([f[0] for f in splt]))
     times = []
     for d in days:
-        hours = [to_pm(f[1]) for f in splt if f[0] == d]
+        hours = [f[1] for f in splt if f[0] == d]
         hours_salas = []
         for h in sorted(set(hours)):
-            salas = hours.count(h)
-            if salas > 1:
-                h = h + (" (%s salas)" % salas)
-            hours_salas.append(h)
+            w_day_hour = weather.day_hour(d, h) if set_weather else None
+            hours_salas.append({
+                "hour": to_pm(h),
+                "weather": w_day_hour,
+                "rooms": hours.count(h),
+                "class": get_class(w_day_hour)
+            })
         times.append({
             "day": d,
             "hours": hours_salas,
             "caption": get_week_day(d)
         })
     return times
+
+def get_cron():
+    cron = CronTab(user=True)
+    comments = []
+    next_run = None
+    for job in cron.find_command(abspath):
+        if job.is_enabled() and job.is_valid():
+            comments.append(job.comment.lower())
+            schedule = job.schedule(date_from=datetime.now())
+            dt = schedule.get_next()
+            if next_run is None or next_run>dt:
+                next_run = dt
+    summary = ", ".join(comments[:-1])
+    if len(comments)>1:
+        summary += " y "+comments[-1]
+    r = {
+        "summary": summary,
+        "next": next_run.strftime("%A %d a las %H:%M").replace(" 0", " ") if next_run else None
+    }
+    return r
 
 paul = get_paul()
 mina = get_mina()
@@ -122,12 +178,23 @@ j2_env = Environment(loader=FileSystemLoader("templates"), trim_blocks=True)
 out = j2_env.get_template('index.html')
 html = out.render(data={
     "now": now.strftime("%A %d a las %H:%M").replace(" 0", " "),
-    "mina": build_times(mina),
-    "paul": build_times(paul),
+    "mina": build_times(mina, set_weather=True),
+    "paul": build_times(paul, set_weather=False),
     "h_min": to_pm(to_hh_mm(H_MIN), short=True),
     "h_max": to_pm(to_hh_mm(H_MAX), short=True),
     "w_min": week[W_MIN],
-    "w_max": week[W_MAX]
+    "w_max": week[W_MAX],
+    "cron": get_cron()
 })
-with open("out/index.html", "wb") as fh:
+with open(out_file, "wb") as fh:
     fh.write(bytes(html, 'UTF-8'))
+
+
+ftp_con = open(".ig_ftp").read().strip()
+host, user, passwd, path = ftp_con.split(" ")
+ftp = FTP(host)
+ftp.login(user=user, passwd=passwd)
+ftp.cwd(path)
+with open(out_file, 'rb') as fh:
+    ftp.storbinary('STOR ' + os.path.basename(out_file), fh)
+ftp.quit()
