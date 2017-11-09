c79aee19df444848c1db74aa2dda3163d81ce93c
s-nt-s <santos82h@gmail.com>
2017-11-09 16:23:51 +0100

    alert

--- out/main.js
+++ out/main.js
@@ -1,5 +1,5 @@
 var next = next_run.split(/-/);
 next = new Date(next[0], next[1] - 1, next[2], next[3], next[4]);
 if (next < new Date()) {
-    alert('El script que genera esta página ha fallado, los datos pueden estar obsoletos');
+    alert('El script que genera esta página esta parado.\nLos datos mostrados pueden estar obsoletos.');
 }
