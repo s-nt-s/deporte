62fab1c15415ee07a1b689218e92e927d9310a09
s-nt-s <santos82h@gmail.com>
2017-11-22 18:09:59 +0100

    weather

--- weather.py
+++ weather.py
@@ -13,17 +13,25 @@ class Weather():
         if isinstance(hour, str):
             hour = int(hour.split(":")[0])
         data = {}
+        periodos = {'estado_cielo'}
         for t in self.tree.findall('.//prediccion/dia[@fecha="' + day + '"]/*'):
             if not t.text:
                 continue
+            periodo_ok = False
             if "periodo" in t.attrib:
+                periodos.add(t.tag)
                 hour1, hour2 = t.attrib["periodo"].split("-")
                 if hour >= int(hour1) or hour < int(hour2):
-                    data[t.tag] = t.text
-                    if t.tag == "viento":
-                        data[t.tag] = t.find("velocidad").text
-                    if "descripcion" in t.attrib:
-                        data[t.tag + "_des"] = t.attrib["descripcion"]
+                    periodo_ok = True
+                    periodos.remove(t.tag)
+            elif t.tag in periodos:
+                periodo_ok = True
+            if periodo_ok:
+                data[t.tag] = t.text
+                if t.tag == "viento":
+                    data[t.tag] = t.find("velocidad").text
+                if "descripcion" in t.attrib:
+                    data[t.tag + "_des"] = t.attrib["descripcion"]
                 continue
             for d in t.findall("dato"):
                 if "hora" in d.attrib:
